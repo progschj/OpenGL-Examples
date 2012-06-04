378421fb2f7959d358057f7c0df3805fc7584d98
Jakob Progsch <jakob@progsch.net>
2012-06-04 13:32:31 +0200

    first commit

---
+++ texture.cpp
@@ -0,0 +1,275 @@
+/* OpenGL example code - Texture
+ * 
+ * apply a texture to the fullscreen quad of "Indexed VBO"
+ * 
+ * Autor: Jakob Progsch
+ */
+
+#include <GL/glew.h>
+#include <GL/glfw.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool running;
+
+// window close callback function
+int closedWindow()
+{
+    running = false;
+    return GL_TRUE;
+}
+
+// helper to check and display for shader compiler errors
+bool check_shader_compile_status(GLuint obj)
+{
+    GLint status;
+    glGetShaderiv(obj, GL_COMPILE_STATUS, &status);
+    if(status == GL_FALSE)
+    {
+        GLint length;
+        glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &length);
+        std::vector<char> log(length);
+        glGetShaderInfoLog(obj, length, &length, &log[0]);
+        std::cerr << &log[0];
+        return false;
+    }
+    return true;
+}
+
+// helper to check and display for shader linker error
+bool check_program_link_status(GLuint obj)
+{
+    GLint status;
+    glGetProgramiv(obj, GL_LINK_STATUS, &status);
+    if(status == GL_FALSE)
+    {
+        GLint length;
+        glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &length);
+        std::vector<char> log(length);
+        glGetProgramInfoLog(obj, length, &length, &log[0]);
+        std::cerr << &log[0];
+        return false;   
+    }
+    return true;
+}
+
+int main()
+{
+    int width = 640;
+    int height = 480;
+    
+    glfwInit();
+
+    // sadly glew doesn't play nice with core profiles... 
+    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+ 
+    // create a window
+    glfwOpenWindow(width, height, 0, 0, 0, 0, 0, 0, GLFW_WINDOW);
+    
+    // setup windows close callback
+    glfwSetWindowCloseCallback(closedWindow);
+    
+    glewExperimental = GL_TRUE;
+    if (GLEW_OK != glewInit())
+    {
+        std::cerr << "failed to init GLEW" << std::endl;
+        return 1;
+    }
+
+    // shader source code
+    std::string vertex_source =
+        "#version 330\n"
+        "in vec4 vposition;\n"
+        "in vec2 vtexcoord;\n"
+        "out vec2 ftexcoord;\n"
+        "void main() {\n"
+        "   ftexcoord = vtexcoord;\n"
+        "   gl_Position = vposition;\n"
+        "}\n";
+        
+    std::string fragment_source =
+        "#version 330\n"
+        "uniform sampler2D texture;\n" // texture uniform
+        "in vec2 ftexcoord;\n"
+        "out vec4 FragColor;\n"
+        "void main() {\n"
+        "   FragColor = texture2D(texture, ftexcoord);\n"
+        "}\n";
+   
+    // program and shader handles
+    GLuint shader_program, vertex_shader, fragment_shader;
+    
+    // we need these to properly pass the strings
+    const char *source;
+    int length;
+
+    // create and compiler vertex shader
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    source = vertex_source.c_str();
+    length = vertex_source.size();
+    glShaderSource(vertex_shader, 1, &source, &length); 
+    glCompileShader(vertex_shader);
+    if(!check_shader_compile_status(vertex_shader))
+    {
+        return 1;
+    }
+ 
+    // create and compiler fragment shader
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    source = fragment_source.c_str();
+    length = fragment_source.size();
+    glShaderSource(fragment_shader, 1, &source, &length);   
+    glCompileShader(fragment_shader);
+    if(!check_shader_compile_status(fragment_shader))
+    {
+        return 1;
+    }
+    
+    // create program
+    shader_program = glCreateProgram();
+    
+    // attach shaders
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+        
+    // bind the attribute locations (inputs)
+    glBindAttribLocation(shader_program, 0, "vposition");
+    glBindAttribLocation(shader_program, 1, "vtexcoord");
+    
+    // bind the FragDataLocation (output)
+    glBindFragDataLocation(shader_program, 0, "FragColor");
+    
+    // link the program and check for errors
+    glLinkProgram(shader_program);
+    check_program_link_status(shader_program);
+    
+    // get texture uniform location
+    GLint texture_location = glGetUniformLocation(shader_program, "texture");
+    
+    // vao and vbo handle
+    GLuint vao, vbo, ibo;
+ 
+    // generate and bind the vao
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    // generate and bind the vertex buffer object
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            
+    // data for a fullscreen quad (this time with texture coords)
+    GLfloat vertexData[] = {
+    //  X     Y     Z           U     V     
+       1.0f, 1.0f, 0.0f,       1.0f, 1.0f, // vertex 0
+      -1.0f, 1.0f, 0.0f,       0.0f, 1.0f, // vertex 1
+       1.0f,-1.0f, 0.0f,       1.0f, 0.0f, // vertex 2
+      -1.0f,-1.0f, 0.0f,       0.0f, 0.0f, // vertex 3
+    }; // 4 vertices with 5 components (floats) each
+
+    // fill with data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*4*5, vertexData, GL_STATIC_DRAW);
+                    
+           
+    // set up generic attrib pointers
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
+ 
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(GLfloat), (char*)0 + 3*sizeof(GLfloat));
+    
+    
+    // generate and bind the index buffer object
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+            
+    GLuint indexData[] = {
+        0,1,2, // first triangle
+        2,1,3, // second triangle
+    };
+
+    // fill with data
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*2*3, indexData, GL_STATIC_DRAW);
+    
+    // "unbind" vao
+    glBindVertexArray(0);
+    
+    // texture handle
+    GLuint texture;
+    
+    // generate texture
+    glGenTextures(1, &texture);
+
+    // bind the texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    // create some image data
+    std::vector<GLubyte> image(4*width*height);
+    for(size_t j = 0;j<height;++j)
+		for(size_t i = 0;i<width;++i)
+		{
+			size_t index = j*width + i;
+			image[4*index + 0] = 0xFF*(j/10%2)*(i/10%2); // R
+			image[4*index + 1] = 0xFF*(j/13%2)*(i/13%2); // G
+			image[4*index + 2] = 0xFF*(j/17%2)*(i/17%2); // B
+			image[4*index + 3] = 0xFF;					 // A
+		}
+    
+    // set texture content
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, &image[0]);
+
+    // set texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	// "unbind" texture
+	glBindTexture(GL_TEXTURE_2D, 0);
+	
+    running = true;
+    while(running)
+    {    
+        // terminate on excape 
+        if(glfwGetKey(GLFW_KEY_ESC))
+        {
+            running = false;
+        }
+        
+        // clear first
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        // use the shader program
+        glUseProgram(shader_program);
+        
+        // bind texture to texture unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        
+        // set texture uniform
+        glUniform1i(texture_location, 0);
+        
+        // bind the vao
+        glBindVertexArray(vao);
+       
+        // draw
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+       
+        // check for errors
+        GLenum error = glGetError();
+        if(error != GL_NO_ERROR)
+        {
+            std::cerr << gluErrorString(error);
+            running = false;       
+        }
+        
+        // finally swap buffers
+        glfwSwapBuffers();       
+    }
+
+    glfwTerminate();
+    return 0;
+}
