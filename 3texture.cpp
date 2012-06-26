564b0713714dae850c6fe5c6289bddf8dcc59840
Jakob Progsch <jakob@progsch.net>
2012-06-26 02:17:03 +0200

    fixed a bug where the texture example requests opengl version 7.3

--- 3texture.cpp
+++ 3texture.cpp
@@ -64,7 +64,7 @@ int main()
 
     // sadly glew doesn't play nice with core profiles... 
     glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 7);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
     glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
  
     // create a window
