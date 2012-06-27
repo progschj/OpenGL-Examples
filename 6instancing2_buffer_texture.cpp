c25b7677b2eb684c3515a979c1d615aa4b9f7482
Jakob Progsch <jakob@progsch.net>
2012-06-27 20:07:56 +0200

    added instancing example with buffer texture

---
+++ 6instancing2_buffer_texture.cpp
@@ -0,0 +1,379 @@
+/* OpenGL example code - Instancing with texture buffer
+ * 
+ * create 8 instances of the cube from the perspective example
+ * the difference to the instancing1 example is that we are
+ * using a texture buffer for the per instance data instead of a
+ * vertex buffer with divisor.
+ * 
+ * Autor: Jakob Progsch
+ */
+#include <GL/glew.h>
+#include <GL/glfw.h>
+ 
+//glm is used to create perspective and transform matrices
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp> 
+ 
+#include <iostream>
+#include <string>
+#include <vector>
+ 
+bool running;
+ 
+// window close callback function
+int closedWindow()
+{
+    running = false;
+    return GL_TRUE;
+}
+ 
+// helper to check and display for shader compiler errors
+bool check_shader_compile_status(GLuint obj)
+{
+    GLint status;
+    glGetShaderiv(obj, GL_COMPILE_STATUS, &status);
+    if(status == GL_FALSE)
+    {
+        GLint length;
+        glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &length);
+        std::vector<char> log(length);
+        glGetShaderInfoLog(obj, length, &length, &log[0]);
+        std::cerr << &log[0];
+        return false;
+    }
+    return true;
+}
+ 
+// helper to check and display for shader linker error
+bool check_program_link_status(GLuint obj)
+{
+    GLint status;
+    glGetProgramiv(obj, GL_LINK_STATUS, &status);
+    if(status == GL_FALSE)
+    {
+        GLint length;
+        glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &length);
+        std::vector<char> log(length);
+        glGetProgramInfoLog(obj, length, &length, &log[0]);
+        std::cerr << &log[0];
+        return false;   
+    }
+    return true;
+}
+ 
+int main()
+{
+    int width = 640;
+    int height = 480;
+    
+    glfwInit();
+ 
+    // sadly glew doesn't play nice with core profiles... 
+    glfwOpenWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);
+    glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);
+ 
+    // create a window
+    glfwOpenWindow(width, height, 0, 0, 0, 0, 0, 0, GLFW_WINDOW);
+    
+    // setup windows close callback
+    glfwSetWindowCloseCallback(closedWindow);
+    
+    glewExperimental = GL_TRUE;
+    GLenum glew_error = glewInit();
+    if (glew_error != GLEW_OK)
+    {
+        std::cerr << "failed to init GLEW: " << glewGetErrorString(glew_error) << std::endl;
+        glfwTerminate();
+        return 1;
+    }
+ 
+    // shader source code
+    std::string vertex_source =
+        "#version 330\n"
+        "uniform mat4 ViewProjection;\n" // the projection matrix uniform
+        "uniform samplerBuffer offset_texture;\n" // the buffer_texture sampler
+        "in vec4 vposition;\n"
+        "in vec4 vcolor;\n"
+        "out vec4 fcolor;\n"
+        "void main() {\n"
+        // access the buffer texture with the InstanceID (tbo[InstanceID])
+        "   vec4 offset = texelFetch(offset_texture, gl_InstanceID);\n" 
+        "   fcolor = vcolor;\n"
+        "   gl_Position = ViewProjection*(vposition + vec4(offset.xyz, 0));\n"
+        "}\n";
+        
+    std::string fragment_source =
+        "#version 330\n"
+        "in vec4 fcolor;\n"
+        "out vec4 FragColor;\n"
+        "void main() {\n"
+        "   FragColor = fcolor;\n"
+        "}\n";
+   
+    // program and shader handles
+    GLuint shader_program, vertex_shader, fragment_shader;
+    
+    // we need these to properly pass the strings
+    const char *source;
+    int length;
+ 
+    // create and compiler vertex shader
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    source = vertex_source.c_str();
+    length = vertex_source.size();
+    glShaderSource(vertex_shader, 1, &source, &length); 
+    glCompileShader(vertex_shader);
+    if(!check_shader_compile_status(vertex_shader))
+    {
+        return 1;
+    }
+ 
+    // create and compiler fragment shader
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    source = fragment_source.c_str();
+    length = fragment_source.size();
+    glShaderSource(fragment_shader, 1, &source, &length);   
+    glCompileShader(fragment_shader);
+    if(!check_shader_compile_status(fragment_shader))
+    {
+        return 1;
+    }
+    
+    // create program
+    shader_program = glCreateProgram();
+    
+    // attach shaders
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+        
+    // bind the attribute locations (inputs)
+    glBindAttribLocation(shader_program, 0, "vposition");
+    glBindAttribLocation(shader_program, 1, "vcolor");
+    
+    // bind the FragDataLocation (output)
+    glBindFragDataLocation(shader_program, 0, "FragColor");
+    
+    // link the program and check for errors
+    glLinkProgram(shader_program);
+    check_program_link_status(shader_program);
+    
+    // obtain location of projection uniform
+    GLint ViewProjection_location = glGetUniformLocation(shader_program, "ViewProjection");
+    GLint offset_texture_location = glGetUniformLocation(shader_program, "offset_texture");
+    
+    
+    // vao and vbo handles
+    GLuint vao, vbo, tbo, ibo;
+ 
+    // generate and bind the vao
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    
+    // generate and bind the vertex buffer object
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+            
+    // data for a cube
+    GLfloat vertexData[] = {
+    //  X     Y     Z           R     G     B
+    // face 0:
+       1.0f, 1.0f, 1.0f,       1.0f, 0.0f, 0.0f, // vertex 0
+      -1.0f, 1.0f, 1.0f,       1.0f, 0.0f, 0.0f, // vertex 1
+       1.0f,-1.0f, 1.0f,       1.0f, 0.0f, 0.0f, // vertex 2
+      -1.0f,-1.0f, 1.0f,       1.0f, 0.0f, 0.0f, // vertex 3
+ 
+    // face 1:
+       1.0f, 1.0f, 1.0f,       0.0f, 1.0f, 0.0f, // vertex 0
+       1.0f,-1.0f, 1.0f,       0.0f, 1.0f, 0.0f, // vertex 1
+       1.0f, 1.0f,-1.0f,       0.0f, 1.0f, 0.0f, // vertex 2
+       1.0f,-1.0f,-1.0f,       0.0f, 1.0f, 0.0f, // vertex 3
+ 
+    // face 2:
+       1.0f, 1.0f, 1.0f,       0.0f, 0.0f, 1.0f, // vertex 0
+       1.0f, 1.0f,-1.0f,       0.0f, 0.0f, 1.0f, // vertex 1
+      -1.0f, 1.0f, 1.0f,       0.0f, 0.0f, 1.0f, // vertex 2
+      -1.0f, 1.0f,-1.0f,       0.0f, 0.0f, 1.0f, // vertex 3
+      
+    // face 3:
+       1.0f, 1.0f,-1.0f,       1.0f, 1.0f, 0.0f, // vertex 0
+       1.0f,-1.0f,-1.0f,       1.0f, 1.0f, 0.0f, // vertex 1
+      -1.0f, 1.0f,-1.0f,       1.0f, 1.0f, 0.0f, // vertex 2
+      -1.0f,-1.0f,-1.0f,       1.0f, 1.0f, 0.0f, // vertex 3
+ 
+    // face 4:
+      -1.0f, 1.0f, 1.0f,       0.0f, 1.0f, 1.0f, // vertex 0
+      -1.0f, 1.0f,-1.0f,       0.0f, 1.0f, 1.0f, // vertex 1
+      -1.0f,-1.0f, 1.0f,       0.0f, 1.0f, 1.0f, // vertex 2
+      -1.0f,-1.0f,-1.0f,       0.0f, 1.0f, 1.0f, // vertex 3
+ 
+    // face 5:
+       1.0f,-1.0f, 1.0f,       1.0f, 0.0f, 1.0f, // vertex 0
+      -1.0f,-1.0f, 1.0f,       1.0f, 0.0f, 1.0f, // vertex 1
+       1.0f,-1.0f,-1.0f,       1.0f, 0.0f, 1.0f, // vertex 2
+      -1.0f,-1.0f,-1.0f,       1.0f, 0.0f, 1.0f, // vertex 3
+    }; // 6 faces with 4 vertices with 6 components (floats)
+ 
+    // fill with data
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*6*4*6, vertexData, GL_STATIC_DRAW);
+                    
+           
+    // set up generic attrib pointers
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (char*)0 + 0*sizeof(GLfloat));
+ 
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(GLfloat), (char*)0 + 3*sizeof(GLfloat));
+ 
+    
+    
+    
+    // generate and bind the index buffer object
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+            
+    GLuint indexData[] = {
+        // face 0:
+        0,1,2,      // first triangle
+        2,1,3,      // second triangle
+        // face 1:
+        4,5,6,      // first triangle
+        6,5,7,      // second triangle
+        // face 2:
+        8,9,10,     // first triangle
+        10,9,11,    // second triangle
+        // face 3:
+        12,13,14,   // first triangle
+        14,13,15,   // second triangle
+        // face 4:
+        16,17,18,   // first triangle
+        18,17,19,   // second triangle
+        // face 5:
+        20,21,22,   // first triangle
+        22,21,23,   // second triangle
+    };
+ 
+    // fill with data
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*6*2*3, indexData, GL_STATIC_DRAW);
+    
+    // "unbind" vao
+    glBindVertexArray(0);
+    
+    
+    // generate and bind the buffer object containing the
+    // instance offsets
+    glGenBuffers(1, &tbo);
+    glBindBuffer(GL_TEXTURE_BUFFER, tbo);
+            
+    // the offsets
+    GLfloat translationData[] = {
+                 2.0f, 2.0f, 2.0f,  // cube 0
+                 2.0f, 2.0f,-2.0f,  // cube 1
+                 2.0f,-2.0f, 2.0f,  // cube 2
+                 2.0f,-2.0f,-2.0f,  // cube 3
+                -2.0f, 2.0f, 2.0f,  // cube 4
+                -2.0f, 2.0f,-2.0f,  // cube 5
+                -2.0f,-2.0f, 2.0f,  // cube 6
+                -2.0f,-2.0f,-2.0f,  // cube 7
+    }; // 8 offsets with 3 components each
+ 
+    // fill with data
+    glBufferData(GL_TEXTURE_BUFFER, sizeof(GLfloat)*3*8, translationData, GL_STATIC_DRAW);
+
+    // texture handle
+    GLuint buffer_texture;
+    
+    // generate and bind the buffer texture
+    glGenTextures(1, &buffer_texture);
+    glBindTexture(GL_TEXTURE_BUFFER, buffer_texture);
+    
+    // tell the buffer texture to use 
+    glTexBuffer(GL_TEXTURE_BUFFER, GL_RGB32F, tbo);
+    
+    // "unbind" texture
+    glBindTexture(GL_TEXTURE_BUFFER, 0);
+    
+    // we are drawing 3d objects so we want depth testing
+    glEnable(GL_DEPTH_TEST);
+ 
+ 
+    running = true;
+    while(running)
+    {   
+        // get the time in seconds
+        float t = glfwGetTime();
+        
+        // terminate on excape 
+        if(glfwGetKey(GLFW_KEY_ESC))
+        {
+            running = false;
+        }
+        
+        // clear first
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        // use the shader program
+        glUseProgram(shader_program);
+        
+        // calculate ViewProjection matrix
+        glm::mat4 Projection = glm::perspective(90.0f, 4.0f / 3.0f, 0.1f, 100.f);
+        
+        // translate the world/view position
+        glm::mat4 View = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+        
+        // make the camera rotate around the origin
+        View = glm::rotate(View, 90.0f*t, glm::vec3(1.0f, 1.0f, 1.0f)); 
+        
+        glm::mat4 ViewProjection = Projection*View;
+       
+        // bind texture to texture unit 0
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_BUFFER, buffer_texture);
+         
+         
+        // set the matrix uniform
+        glUniformMatrix4fv(ViewProjection_location, 1, GL_FALSE, glm::value_ptr(ViewProjection)); 
+        
+        // set texture uniform
+        glUniform1i(offset_texture_location, 0);
+    
+        
+        // bind the vao
+        glBindVertexArray(vao);
+
+       
+        // draw
+        // the additional parameter indicates how many instances to render
+        glDrawElementsInstanced(GL_TRIANGLES, 6*6, GL_UNSIGNED_INT, 0, 8);
+       
+        // check for errors
+        GLenum error = glGetError();
+        if(error != GL_NO_ERROR)
+        {
+            std::cerr << gluErrorString(error);
+            running = false;       
+        }
+        
+        // finally swap buffers
+        glfwSwapBuffers();       
+    }
+    
+    // delete the created objects
+        
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glDeleteBuffers(1, &ibo);
+    glDeleteBuffers(1, &tbo);
+    
+    glDeleteTextures(1, &buffer_texture);
+    
+    glDetachShader(shader_program, vertex_shader);      
+    glDetachShader(shader_program, fragment_shader);
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+    glDeleteProgram(shader_program);
+    
+    glfwTerminate();
+    return 0;
+}
